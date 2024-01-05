8e2604633834f769c3c7f6f15ef6ec0a711a9fe6
Daniel Wang <dan-wang@live.com>
2024-01-04 23:00:39 -0800

    fix css for base model

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ icons = document.querySelectorAll(".row i");
 translateBtn = document.querySelector("button"),
 selectTag.forEach((tag, id) => {
     for (let country_code in countries) {
-        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "en-GB" ? "selected" : "";
         let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option);
     }
