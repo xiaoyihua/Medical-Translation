89984546ab952d22f489b7311f3cdb58c8b6b8a6
xiaoyihua <dan-wang@live.com>
2024-04-29 01:33:47 -0700

    added comments

--- DatasetExplorer.py
+++ DatasetExplorer.py
@@ -1,3 +1,29 @@
+"""
+Program that parses and formats files from MeSpEn dataset into CSV files for further exploration.
+
+Main Functions:
+- format_xml (string --> dataframe):
+    - Takes in string of os path, reads file of type xml
+    - Uses xml schema to extract matched english and spanish titles and descriptions for one file
+
+- create_dataframes (string --> dataframe):
+    - Takes in string of relative path to directory folder
+    - Loops format_xml and combines dataframes into one
+
+- count_sentences (df --> df):
+    - creates new column in df based on sentences found using punkt's sentence tokenizer
+    - punkt's sentence tokenizer is built on an unsupervised learning model (unspecified)
+    - punkt's tokenizer needs to have its language set as an option
+
+- count_words (df --> df):
+    - creates new columns in df based on words in paragraph using python's split
+
+Initial Author: Daniel Wang
+Additional Authors: 
+Last Refactored: Daniel Wang
+Last Tested: 
+"""
+
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -10,10 +36,23 @@ import nltk
 import matplotlib.pyplot as plt
 from nltk.tokenize import sent_tokenize
 
-def format_xml(path):
+__author__ = '{author}'
+__copyright__ = 'Copyright {year}, {project_name}'
+__credits__ = ['{credit_list}']
+__license__ = '{license}'
+__version__ = '{mayor}.{minor}.{rel}'
+__maintainer__ = '{maintainer}'
+__email__ = '{contact_email}'
+__status__ = '{dev_status}'
+
+def format_xml(path, source):
+    # Given string path, read one xml file
+    # Source to create source column - Equivalent to dir
     txt = Path(path).read_text()
     xml_data = StringIO(txt)
 
+    # Take advantage of xml schemas to extract the fields we want - located in xpath
+    # Each df produced by this method returns en title and es title as separate rows
     df = pd.read_xml(xml_data, 
                     xpath="//dc:title", 
                     namespaces={"dc": "http://purl.org/dc/elements/1.1/", "dcterms":"http://purl.org/dc/terms/", 
@@ -21,6 +60,7 @@ def format_xml(path):
                     parser="lxml")
     df = df.convert_dtypes(convert_string=True)
 
+    # Must reopen file and rextract to get the other fields
     txt = Path(path).read_text()
     xml_data = StringIO(txt)
 
@@ -30,30 +70,33 @@ def format_xml(path):
                                                             "xsi": "http://www.w3.org/2001/XMLSchema-instance"},
                     parser="lxml")
     
+    # Dataframe formatting to make sure they are the same types for joining
     df2 = df2.convert_dtypes(convert_string=True)
     df = df.merge(df2, on='lang', how='left')
 
+    # Dataframe operations to reshape the data into single rows
     df.set_index('lang', inplace=True)
     data = {'title_en': [df['title']['en']], 'title_es': [df['title']['es']], 'description_en': [df['description']['en']], 'description_es': [df['description']['es']]}
     df = pd.DataFrame.from_dict(data)
 
-    df['Source'] = 'MedlinePlus'
+    df['Source'] = f'{source}'
     df['File'] = f'{path}'
 
     return df
 
 def create_dataframes(dir):
     directory = dir
-
     dataframes = []
     counter = 0
 
+    # Main loop that iterates through each file in the directory
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
         # checking if it is a file
+        # if it is a file, format using above function and store dataframe in a list
         if os.path.isfile(f):
             try:
-                df = format_xml(f)
+                df = format_xml(f, dir)
                 dataframes.append(df)
             except (UnicodeDecodeError, ValueError, KeyError):
                 print(f)
@@ -61,17 +104,20 @@ def create_dataframes(dir):
 
     print(counter)
 
+    # connect all dataframes in list together - faster than joining dataframes at every step
     final = pd.concat(dataframes, ignore_index=True)
 
     return final
 
 def count_words(df):
+    # Counting words using split - slightly inflated by punctuation but good enough
     df['NumWordsEn'] = df.apply(lambda x: len((str(x['description_en'])).split()), axis=1)
     df['NumWordsEs'] = df.apply(lambda x: len((str(x['description_es'])).split()), axis=1)
 
     return df
 
 def count_sentences(df):
+    # Counting sentences using nltk setn_tokenize, which uses punkt
     df['NumSentencesEn'] = df.apply(lambda x: len(sent_tokenize(x['description_en'], language='english')), axis=1)
     df['NumSentencesEs'] = df.apply(lambda x: len(sent_tokenize(x['description_es'], language='spanish')), axis=1)
 
